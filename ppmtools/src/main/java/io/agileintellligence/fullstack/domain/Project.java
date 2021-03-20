15c630065d7f18c1ff99f8b00462ced5ec1d5cb0
AmarNaath M <amarnaath@AmarNaaths-MacBook-Pro.local>
2021-03-20 11:48:11 +0530

    Project Object

--- ppmtools/src/main/java/io/agileintellligence/fullstack/domain/Project.java
+++ ppmtools/src/main/java/io/agileintellligence/fullstack/domain/Project.java
@@ -1,4 +1,29 @@
 package io.agileintellligence.fullstack.domain;
 
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    @CreationTimestamp
+    private Date created_At;
+    @UpdateTimestamp
+    private Date updated_At;
+
+    public Project() {
+    }
 }
