e6b5e314bebe13f6f922d20e760c594d8ed4d0f8
AmarNaath M <amarnaath@AmarNaaths-MacBook-Pro.local>
2021-03-21 09:45:53 +0530

    Project Object

--- ppmtools/src/main/java/io/agileintellligence/fullstack/web/ProjectController.java
+++ ppmtools/src/main/java/io/agileintellligence/fullstack/web/ProjectController.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/project")
@@ -22,7 +26,12 @@ public class ProjectController {
     {
         if(result.hasErrors())
         {
-            return new ResponseEntity<String>("INVALID PROJECT",HttpStatus.BAD_REQUEST);
+            Map<String, String> mp = new HashMap<String, String>();
+            for(FieldError error:result.getFieldErrors())
+            {
+               mp.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String, String>>(mp,HttpStatus.BAD_REQUEST);
         }
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<>(project1, HttpStatus.CREATED);
