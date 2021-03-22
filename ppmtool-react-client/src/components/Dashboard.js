e52308fffbc7feba3d3b06b9fdc83288be4bddac
AmarNaath M <amarnaath@AmarNaaths-MacBook-Pro.local>
2021-03-22 10:39:28 +0530

    Project Object

---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import ProjectItem from './Project/ProjectItem';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+        <h1 className="alert alert-warning">WELOCME PPL</h1>
+             <ProjectItem />
+             <ProjectItem />
+             <ProjectItem />
+
+     </div>
+        );
+    }
+}
+
+export default Dashboard;
---
+++ ppmtool-react-client/src/components/Project/ProjectItem.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class ProjectItem extends Component {
+    render() {
+        return (
+           <h1>Project Items</h1>
+        )
+    }
+}
+export default ProjectItem;
