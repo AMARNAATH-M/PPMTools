615324c77ecc8191029b155628725b1d2ef6e7c1
AmarNaath M <amarnaath@AmarNaaths-MacBook-Pro.local>
2021-03-25 10:14:43 +0530

    Project Object

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,27 +1,41 @@
-import React, { Component } from 'react'
-import CreateProjectButton from './Project/CreateProjectButton';
-import ProjectItem from './Project/ProjectItem';
+import React, { Component } from "react";
+import CreateProjectButton from "./Project/CreateProjectButton";
+import ProjectItem from "./Project/ProjectItem";
+import { connect } from "react-redux";
+import { getProjects } from "../actions/projectAction";
+import PropTypes from "prop-types";
 
 class Dashboard extends Component {
-    render() {
-        return (
-            <div className="projects">
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-12">
-                        <h1 className="display-4 text-center">Projects</h1>
-                        <br />
-                        <CreateProjectButton></CreateProjectButton>
-                        <br />
-                        <hr />
-    <ProjectItem></ProjectItem>
-                    </div>
-                </div>
+  componentDidMount() {
+    this.props.getProjects();
+  }
+  render() {
+    return (
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <CreateProjectButton></CreateProjectButton>
+              <br />
+              <hr />
+              <ProjectItem></ProjectItem>
             </div>
+          </div>
         </div>
-    
-        );
-    }
+      </div>
+    );
+  }
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired,
+};
+
+// const mapStateToProps = (state) => ({
+//   project: state.project,
+// });
+
+export default connect(null, { getProjects })(Dashboard);
