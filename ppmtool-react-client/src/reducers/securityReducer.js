e0d8239f928e37382481a7b9822264fa4fb33afa
AmarNaath M <amarnaath@AmarNaaths-MacBook-Pro.local>
2021-04-11 13:28:53 +0530

    Project Object

---
+++ ppmtool-react-client/src/reducers/securityReducer.js
@@ -0,0 +1,26 @@
+import { SET_CURRENT_USER } from "../actions/type";
+
+const initialState = {
+  user: {},
+  validToken: false,
+};
+const booleanActionPayload = (payload) => {
+  if (payload) {
+    return true;
+  } else {
+    return false;
+  }
+};
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        validToken: true,
+        user: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
